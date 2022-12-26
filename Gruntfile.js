d7b16e8da3b4710d5e40ec7080657ce32f43973a
Amit Singh <apprimit@gmail.com>
2022-12-26 15:18:51 +0530

    language pot file regenerated

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,7 @@ module.exports = function (grunt) {
     require("time-grunt")(grunt);
 
     // Load all Grunt tasks
-    require("jit-grunt")(grunt, {
-        makepot: "grunt-wp-i18n",
-    });
+    require("jit-grunt")(grunt, {});
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
@@ -118,6 +116,9 @@ module.exports = function (grunt) {
                     "!node_modules/**",
                     "!build/**",
                     "!.git/**",
+                    "!vendor/**",
+                    "!composer.json",
+                    "!composer.lock",
                     "!Gruntfile.js",
                     "!package.json",
                     "!package-lock.json",
@@ -146,40 +147,6 @@ module.exports = function (grunt) {
                 cwd: "build/<%= pkg.name %>/",
                 src: ["**/*"],
                 dest: "<%= pkg.name %>/",
-            },
-        },
-
-        makepot: {
-            target: {
-                options: {
-                    domainPath: "/languages/", // Where to save the POT file.
-                    exclude: [
-                        // Exlude folder.
-                        "build/.*",
-                        "assets/.*",
-                        "readme/.*",
-                        "sass/.*",
-                        "bower_components/.*",
-                        "node_modules/.*",
-                    ],
-                    potFilename: "<%= pkg.name %>.pot", // Name of the POT file.
-                    type: "wp-plugin", // Type of project (wp-plugin or wp-theme).
-                    updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
-                    processPot: function (pot, options) {
-                        pot.headers["plural-forms"] = "nplurals=2; plural=n != 1;";
-                        pot.headers["last-translator"] = "OceanWP\n";
-                        pot.headers["language-team"] = "OceanWP\n";
-                        pot.headers["x-poedit-basepath"] = "..\n";
-                        pot.headers["x-poedit-language"] = "English\n";
-                        pot.headers["x-poedit-country"] = "UNITED STATES\n";
-                        pot.headers["x-poedit-sourcecharset"] = "utf-8\n";
-                        pot.headers["x-poedit-searchpath-0"] = ".\n";
-                        pot.headers["x-poedit-keywordslist"] =
-                            "_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n";
-                        pot.headers["x-textdomain-support"] = "yes\n";
-                        return pot;
-                    },
-                },
             },
         },
     });
