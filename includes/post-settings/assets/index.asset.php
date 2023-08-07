f95ded51af95d6f112c519c6752068535553f61d
Amit Singh <apprimit@gmail.com>
2023-08-07 16:44:08 +0530

    bug fix, metabox improvements

--- includes/post-settings/assets/index.asset.php
+++ includes/post-settings/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => 'd8c882f8851cc086f978');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '58c25b9c54267115f9a1');
