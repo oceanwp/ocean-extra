b75547a62f8c9101cc864f6dfb219377161e1a63
Amit Singh <apprimit@gmail.com>
2023-08-10 17:31:57 +0530

    remove classic editor support for ocean_library

--- includes/post-settings/assets/index.asset.php
+++ includes/post-settings/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '5bb62d4564ddcaf431b0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '04da3d01dceada875206');
