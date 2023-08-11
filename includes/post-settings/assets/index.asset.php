0de7de2f2425ebe1350d89400399ff23d311797d
Amit Singh <apprimit@gmail.com>
2023-08-11 16:55:33 +0530

    metabox panel priority

--- includes/post-settings/assets/index.asset.php
+++ includes/post-settings/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '91fbd80ffead3ee1e861');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => 'a9ecfa2392109de0e68d');
