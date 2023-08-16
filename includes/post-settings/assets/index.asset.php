8af67446f984d73d48828db82bba658bb96fab48
Amit Singh <apprimit@gmail.com>
2023-08-16 17:03:18 +0530

    metabox panel order and style

--- includes/post-settings/assets/index.asset.php
+++ includes/post-settings/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => 'a7e9374b9bcf11a403bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '8c7922cf7f6a8da4345d');
