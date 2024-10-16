ac21bd458e1f500390d9b278a01d2de14540c2dd
Amit Singh <apprimit@gmail.com>
2024-10-16 15:38:57 +0530

    version + changelog

--- ocean-extra.php
+++ ocean-extra.php
@@ -7,7 +7,7 @@
  * Author:              OceanWP
  * Author URI:          https://oceanwp.org/
  * Requires at least:   5.6
- * Tested up to:        6.6.2
+ * Tested up to:        6.6
  * Text Domain: ocean-extra
  * Domain Path: /languages
  *
