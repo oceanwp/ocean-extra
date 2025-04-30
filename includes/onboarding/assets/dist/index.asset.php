25fc3a5cbb80cdce62292dc14abbd892cfd63bf9
Amit Singh <apprimit@gmail.com>
2025-04-30 16:48:52 +0530

    onboarding process (12042)

--- includes/onboarding/assets/dist/index.asset.php
+++ includes/onboarding/assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e97415ec8c66f53cb170');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '62ea8d6211fb5aaa528b');
