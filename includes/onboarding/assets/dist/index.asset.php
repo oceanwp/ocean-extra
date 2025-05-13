64448a1706e1060280ff73cbe7899e3c73c8afe1
Amit Singh <apprimit@gmail.com>
2025-05-13 14:24:51 +0530

    styling and template filter

--- includes/onboarding/assets/dist/index.asset.php
+++ includes/onboarding/assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'af4ba8a8995ad662a8f8');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4085eea979a8c8a2108f');
