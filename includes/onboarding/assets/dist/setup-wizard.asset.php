79071c3bdb7668efe6c68496896ba2463fcc92db
Amit Singh <apprimit@gmail.com>
2025-07-04 14:19:28 +0530

    regular import (12433)

---
+++ includes/onboarding/assets/dist/setup-wizard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd29f3eaa35352469a60d');
