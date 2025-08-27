dfcca5436a9bb3c8861ffd33a58c07d608d1c82a
Amit Singh <apprimit@gmail.com>
2025-08-27 13:13:12 +0530

    code refactor (13031)

--- includes/panel/library-shortcode.php
+++ includes/panel/library-shortcode.php
@@ -33,66 +33,70 @@ if ( ! class_exists( 'OceanWP_Library_Shortcode' ) ) {
 				'id' => '',
 			), $atts, 'oceanwp_library' );
 
+			$id = absint( $atts['id'] );
+
+			if ( ! $id ) {
+				return '';
+			}
+
+			$owp_post_type   = get_post_type( $id );
+			$owp_post_status = get_post_status( $id );
+
+			if ( $owp_post_type !== 'oceanwp_library' || $owp_post_status !== 'publish' ) {
+				return '';
+			}
+
 			ob_start();
 
-			if ( $atts[ 'id' ] ) {
+			// Check if the template is created with Elementor
+			$elementor = get_post_meta( $id, '_elementor_edit_mode', true );
 
-				$owp_post_type = get_post_type( intval( $atts[ 'id' ] ) );
-				$owp_post_status = get_post_status( intval( $atts[ 'id' ] ) );
+			// If Elementor
+			if ( class_exists( 'Elementor\Plugin' ) && $elementor ) {
 
-				if ( $owp_post_type !== 'oceanwp_library' || $owp_post_status !== 'publish' ) {
-					return;
-				}
+				echo Elementor\Plugin::instance()->frontend->get_builder_content_for_display( $id );
 
-				// Check if the template is created with Elementor
-				$elementor  = get_post_meta( $atts[ 'id' ], '_elementor_edit_mode', true );
+			}
 
-				// If Elementor
-				if ( class_exists( 'Elementor\Plugin' ) && $elementor ) {
+			// If Beaver Builder.
+			else if ( class_exists( 'FLBuilder' ) && ! empty( $id ) ) {
 
-					echo Elementor\Plugin::instance()->frontend->get_builder_content_for_display( $atts[ 'id' ] );
+				echo do_shortcode( '[fl_builder_insert_layout id="' . esc_attr( $id )  . '"]' );
 
-				}
+			}
 
-				// If Beaver Builder.
-				else if ( class_exists( 'FLBuilder' ) && ! empty( $atts[ 'id' ] ) ) {
+			// if SiteOrigin.
+			else if ( class_exists( 'SiteOrigin_Panels' ) && get_post_meta( $id, 'panels_data', true ) ) {
 
-					echo do_shortcode( '[fl_builder_insert_layout id="' . $atts[ 'id' ] . '"]' );
+				echo SiteOrigin_Panels::renderer()->render( $id );
 
-				}
+			}
 
-				// if SiteOrigin.
-				else if ( class_exists( 'SiteOrigin_Panels' ) && get_post_meta( $atts[ 'id' ], 'panels_data', true ) ) {
+			// Else
+			else {
 
-					echo SiteOrigin_Panels::renderer()->render( $atts[ 'id' ] );
+				// Get template content
+				$content = '';
 
-				}
+				if ( ! empty( $id ) ) {
 
-				// Else
-				else {
+					$template = get_post( $id );
 
-					// Get template content
-					$content = $atts[ 'id' ];
-
-					if ( ! empty( $content ) ) {
-
-						$template = get_post( intval( $content ) );
-
-						if ( is_object( $template ) && ! is_wp_error( $template ) ) {
-							$content = $template->post_content;
-						}
-
+					if ( is_object( $template ) && ! is_wp_error( $template ) ) {
+						$content = $template->post_content;
 					}
 
 					// If Gutenberg.
-					if ( ocean_is_block_template( $atts[ 'id' ] ) ) {
+					if ( function_exists( 'ocean_is_block_template' ) && ocean_is_block_template( $id ) ) {
 						$content = apply_filters( 'oe_library_shortcode_template_content', do_blocks( $content ) );
 					}
-
-					// Display template content.
-					echo do_shortcode( $content );
-
 				}
+
+				// Display template content.
+				if ( ! empty( $content ) ) {
+					echo do_shortcode( $content );
+				}
+
 			}
 
 			return ob_get_clean();
